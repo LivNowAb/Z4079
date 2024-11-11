326e0d7e4a4d74b31dfc5c2cd298ae94f0097419
Liviczech <livia.abdul@yahoo.com>
2024-11-11 19:49:24 +0100

    Initial commit

--- kalkulacka.py
+++ kalkulacka.py
@@ -1,21 +1,24 @@
-prvni_cislo = float(input("Zadejte prvni cislo: "))
-znamenko = input("Zadejte znamenko (+, -, *, /): ")
-druhe_cislo = float(input("Zadejte druhe cislo: "))
+try:
+    prvni_cislo = float(input("Zadejte prvni cislo: "))
+    znamenko = input("Zadejte znamenko (+, -, *, /): ")
+    druhe_cislo = float(input("Zadejte druhe cislo: "))
 
 
-if znamenko == '+':
-    vysledek = prvni_cislo + druhe_cislo
-elif znamenko == '-':
-    vysledek = prvni_cislo - druhe_cislo
-elif znamenko == '*':
-    vysledek = prvni_cislo * druhe_cislo
-elif znamenko == '/':
-    try:
-        vysledek = prvni_cislo / druhe_cislo
-    except ZeroDivisionError:
-        print("Nulou nelze delit.")
-else:
-    vysledek = "Chyba."
+    if znamenko == '+':
+        vysledek = prvni_cislo + druhe_cislo
+    elif znamenko == '-':
+        vysledek = prvni_cislo - druhe_cislo
+    elif znamenko == '*':
+        vysledek = prvni_cislo * druhe_cislo
+    elif znamenko == '/':
+        try:
+            vysledek = prvni_cislo / druhe_cislo
+        except ZeroDivisionError:
+            print("Nulou nelze delit.")
+    else:
+        vysledek = "Chyba."
 
+    print(f"Vysledek je {vysledek}.")
 
-print(f"Vysledek je {vysledek}.")
+except ValueError:
+    print("Chyba: Zadejte cislo.")
