ed1e87bbef950001b0fc4e52ffcfb8692168c954
Liviczech <livia.abdul@yahoo.com>
2024-11-12 18:36:47 +0100

    dalsi zmena k zapisu

---
+++ Classes.py
@@ -0,0 +1,42 @@
+class Animals:
+    def __init__(self, weight, age):
+        self.weight = weight
+        self.age = age
+
+    def look(self):
+        print(f"Looking at you...")
+
+    def breath(self):
+        print(f"Breathing normally when not active.")
+
+class Fish(Animals):
+
+    def swim(self):
+        print("Swimming in the sea.")
+
+class Bird(Animals):
+
+    def fly(self):
+        print("Flying high, feeling free...")
+
+class Mammal(Animals):
+
+    def run(self):
+        print("Running fast when needed.")
+
+class DomesticDog(Mammal):
+
+    def __init__(self, weight, age, breed, coat_color):
+        super().__init__(weight, age)
+        self.breed = breed
+        self.coat_color = coat_color
+
+    def bark(self):
+        print(f"Barking at a postman or just for fun.")
+
+    def retrieve(self):
+        print(f"Retrieving everything back to the owner.")
+
+dd1 = DomesticDog(30, 7, "german shepherd", "brown")
+dd1.bark()
+print(dd1.breed)
