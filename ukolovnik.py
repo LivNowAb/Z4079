b822c7fb5ea20858a0136dd1020018dd1a0ff7f0
Liviczech <livia.abdul@yahoo.com>
2024-11-14 20:47:27 +0100

    struktura

---
+++ ukolovnik.py
@@ -0,0 +1,34 @@
+"""Cílem úkolu je vytvořit poznámkový blok V poznámkovém bloku můžeme přidávat,
+ odebírat nebo měnit řádky. Když spustíme program tak máme následující možnosti:
+
+Přidat poznámku (nakonec)
+Vypsat všechny poznámky
+Smazat poznámku (budeme vyzváni, jaký řádek smazat)
+Upravit poznámku (budeve vyzváni, jaký řádek a jak upravit)
+Uložit poznámky do souboru .csv (budeme vyzváni do jakého souboru)
+(Volitelně uložení i přes pickle)
+Načíst poznámky ze souboru .csv (budeme vyzváni z jakého souboru)"""
+
+class Notes:
+    def __init__(self):
+        pass
+
+    def add_note():
+        pass
+
+    def print_notes(self):
+        pass
+
+    def delete_note():
+        pass
+
+    def edit_note():
+        pass
+
+# ulozit do csv = vyzva do jakeho, import asi nahore a ulozit pres writer
+    def save_csv:
+        pass
+
+# nacist z csv = vyzva z ktereho souboru a nacist pres reader
+    def load_csv:
+        pass
