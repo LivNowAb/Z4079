d583bc271e61f25c78c98e6c7bf23d3c2d84bed7
Liviczech <livia.abdul@yahoo.com>
2024-11-25 19:29:06 +0100

    update pred vyresenim indexu od 1 pro uzivatele

--- ukolovnik.py
+++ ukolovnik.py
@@ -9,7 +9,7 @@ Uložit poznámky do souboru .csv (budeme vyzváni do jakého souboru)
 (Volitelně uložení i přes pickle)
 Načíst poznámky ze souboru .csv (budeme vyzváni z jakého souboru)"""
 
-# import csv asi
+import csv
 
 class Notes:
     def __init__(self):
@@ -18,35 +18,69 @@ class Notes:
     def add_note(self):
         note = input(f"Zadejte svou poznamku zde: ")
         self.notes.append(note)
+        print("Poznamka byla pridana.")
 
-    def print_notes(self, note):
-        if note not in self.notes:
+    def print_notes(self):
+        if not self.notes:
             print(f"V ukolovniku nejsou zadne poznamky")
         else:
             print(self.notes)
 
-    def delete_note(self, num_delete):
+    def delete_note(self):
         num_delete = int(input(f"Zadejte cislo radku s poznamkou, kterou si prejete smazat: "))
         if 0 <= num_delete <= len(self.notes):
-            deleted_note = self.notes.remove(num_delete)
+            deleted_note = self.notes.pop(num_delete)
             print(f"Poznamka cislo {num_delete} byla smazana.")
         else:
             print(f"Pod zadanym cislem neexistuje poznamka.")
 
     def edit_note(self):
-        num_edit = int(input(f"Zadejte cislo poznamky, kterou chcete smazat: "))
+        num_edit = int(input(f"Zadejte cislo poznamky, kterou chcete upravit: "))
         if 0 <= num_edit <= len(self.notes):
-            new_note = input(f"Zadejte novou poznamku misto prave smazane.")
+            new_note = input(f"Zadejte novou poznamku misto puvodni poznamky: ")
             self.notes[num_edit] = new_note
             print(f"Poznamka cislo {num_edit} byla zmenena.")
         else:
             print(f"Pod timto cislem nebyla nalezena zadna poznamka.")
 
-# ulozit do csv = vyzva do jakeho, import asi nahore a ulozit pres writer, tba
     def save_csv(self):
-        pass
+        filename = input("Zadejte nazev souboru pro ulozeni (s priponou .csv): ")
+        with open(filename, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            for note in self.notes:
+                writer.writerow([note])
+        print(f"Poznamky byly ulozeny do souboru '{filename}'.")
 
-# nacist z csv = vyzva z ktereho souboru a nacist pres reader, tba
     def load_csv(self):
-        pass
+        filename = input("Zadejte nazev souboru pro nacteni (s priponou .csv): ")
+        with open(filename, mode='r', newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            self.notes = [row[0] for row in reader]
+        print(f"Poznámky byly načteny ze souboru '{filename}'.")
 
+
+
+def choose_action():
+    notebook = Notes()
+    while True:
+        action = input(
+            f"Vyberte, jaky ukon si prejete s poznamkami provest: pridat, zobrazit, smazat, upravit, ulozit, nacist, ukoncit: ")
+        if action == "pridat":
+            notebook.add_note()
+        elif action == "zobrazit":
+                notebook.print_notes()
+        elif action == "smazat":
+                notebook.delete_note()
+        elif action == "upravit":
+                notebook.edit_note()
+        elif action == "ulozit":
+                notebook.save_csv()
+        elif action == "nacist":
+                notebook.load_csv()
+        elif action == "ukoncit":
+                print(f"Neplecha ukoncena.")
+                break
+        else:
+            print(f"Zadali jste nepovoleny ukon. Zkuste to jeste jednou.")
+
+choose_action()
