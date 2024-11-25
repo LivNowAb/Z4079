efd6063ce1657d6be0851d1b1d16e2e239461bb6
Liviczech <livia.abdul@yahoo.com>
2024-11-25 17:09:55 +0100

    update

--- ukolovnik.py
+++ ukolovnik.py
@@ -9,26 +9,33 @@ Uložit poznámky do souboru .csv (budeme vyzváni do jakého souboru)
 (Volitelně uložení i přes pickle)
 Načíst poznámky ze souboru .csv (budeme vyzváni z jakého souboru)"""
 
+import csv
+
 class Notes:
     def __init__(self):
-        pass
+        self.notes = []
 
-    def add_note():
-        pass
+    def add_note(self):
+        note = input(f"Zadejte svou poznamku zde: ")
+        self.notes.append(note)
 
     def print_notes(self):
-        pass
+        if not notes in self.notes:
+            print(f"V ukolovniku nejsou zadne poznamky")
+        else:
+            print(self.notes)
 
-    def delete_note():
-        pass
+    def delete_note(self):
+        self.print_notes()
+        cislo_smazat = input(f"Zadejte cislo radku s poznamkou, kterou si prejete smazat: ")
 
-    def edit_note():
+    def edit_note(self):
         pass
 
 # ulozit do csv = vyzva do jakeho, import asi nahore a ulozit pres writer
-    def save_csv:
+    def save_csv(self):
         pass
 
 # nacist z csv = vyzva z ktereho souboru a nacist pres reader
-    def load_csv:
+    def load_csv(self):
         pass
